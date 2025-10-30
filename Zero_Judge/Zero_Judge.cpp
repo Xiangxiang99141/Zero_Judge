0f3b10d4d4a683764f8bd0668930ac71349fc424
DogBrother <roys99141@gmail.com>
2025-10-31 01:21:22 +0800

    好耶，過了

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -1,7 +1,15 @@
 ﻿#include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    string input = "";
+	cin >> input;
+	int str_size = input.length();
+	for (int i = 0; i < str_size; i++) {
+		for (int j = 1; j <= str_size-i; j++) { //原始長度-已輸出長度
+			if (i == 0 &&i == j)  continue;
+			cout << input.substr(i, j) << endl;
+		}
+	}
 }
 
