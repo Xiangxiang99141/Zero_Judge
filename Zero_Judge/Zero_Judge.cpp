e477a59ddae5d15e4c9e3d1f8c2c0b54a71d8b5d
DogBrother <roys99141@gmail.com>
2025-10-30 18:12:05 +0800

    這樣居然也行

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -1,7 +1,34 @@
 ﻿#include <iostream>
-
+#include <vector>
+using namespace std;
+vector<int> getMaxMin(vector<int> i);
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<int> numbers;
+    int behine_zero = 0;
+    int input=0;
+	vector<int> max_min;
+    while (cin >> input) {
+        if (input == -9999) break;
+        if (input < 0) behine_zero++;
+        numbers.push_back(input);
+    }
+	max_min = getMaxMin(numbers);
+	cout << max_min[0] << "," << max_min[1] << "," << numbers.size() - behine_zero << "," << behine_zero << endl;
+
+
+    //std::cout << "Hello World!\n";
 }
 
+vector<int> getMaxMin(vector<int> i) {
+    vector<int> t;
+	int max = i[0];
+	int min = i[0];
+	for (int n : i) {
+		if (n > max) max = n;
+		if (n < min) min = n;
+	}
+	t.push_back(max);
+	t.push_back(min);
+    return t ;
+}
