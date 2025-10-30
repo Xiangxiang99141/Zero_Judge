55989a4b13b7ce0723aab75f5360a427bf0212f1
DogBrother <roys99141@gmail.com>
2025-10-31 02:44:31 +0800

    怪了，測試可以過，提交不能過

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -1,7 +1,23 @@
 ﻿#include <iostream>
-
+using namespace std;
+bool isPrime(int number);
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x = 0;
+    while (cin >> x) {
+        if (x == 1) {
+			cout << x << "不是質數" << endl;
+        }
+		else cout << x << (isPrime(x) ? "是質數" : "不是質數") << endl;
+    }
+    
+}
+bool isPrime(int number)
+{
+    for (int i = 2; i < number - 1; i++) {
+		if (number % i == 0)
+			return false;
+    }
+    return true;
 }
 
