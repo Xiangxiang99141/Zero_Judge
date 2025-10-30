abcb04b9a6868e1cc18ee2be69012c83e742817f
DogBrother <roys99141@gmail.com>
2025-10-30 16:53:53 +0800

    成功拉

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -1,7 +1,54 @@
-﻿#include <iostream>
+﻿/*
+讓使用者輸入一個正整數 n，接著依底下規則輸出一行字串。
 
+- 從數字1到n，依序出現在字串中
+
+- 每個數字的後面會連接三種符號（+，-，*）之一
+
+- 連接的規則是根據該數字除以3的餘數去決定
+
+- 當數字是3的倍數餘1，則連接一個 +
+
+- 當數字是3的倍數餘2，則連接二個 –
+
+- 當數字是3的倍數，則連接三個 *
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void displayResult(int i);
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a = 0;
+    while (cin >> a) {
+        displayResult(a);
+    }
 }
+
+void displayResult(int a) {
+    string result = "";
+
+    for (int i = 1; i <= a; i++)
+    {
+        switch (i % 3)
+        {
+        case 0:
+            result += to_string(i) + "***";
+            break;
+        case 1:
+            result += to_string(i) + "+";
+            break;
+        case 2:
+            result += to_string(i) + "--";
+            break;
+        default:
+            break;
+        }
+    }
+    std::cout << result << endl;
+}
+
+
 
