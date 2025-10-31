89d50a1cdcb2dbac820623f0a084c76bb5e1967d
DogBrother <roys99141@gmail.com>
2025-10-31 22:47:37 +0800

    還是稿不定

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <vector>
 #include <string>
+#include <sstream>
 #include <iomanip>
 using namespace std;
 void split(const string&, const string&);
@@ -10,44 +11,41 @@ int main()
     string input = "";
 	while (getline(cin,input)) {
 		split(input," ");
+		//cin.ignore();
 	}
 
 }
 
 void split(const string& str, const string& delim = " ") {
-	vector<float> parts;
-	size_t start = 0;
-	size_t end = str.find(delim);
-	while (end != string::npos) {
-		parts.push_back(stoi(str.substr(start, end-start)));
-		start = end + delim.length();
-		end = str.find(delim, start);
+	vector<int> parts;
+	stringstream ss(str);
+	string word;
+	while (ss >> word) {
+		parts.push_back(stoi(word));
 	}
-	parts.push_back(stoi(str.substr(start, end)));
-	
 	/*計算bmi*/
-	double bmi = parts[1] / pow((parts[0] / 100), 2);
+	double height = parts[0] / 100.0;
+	double bmi = parts[1] / pow(height , 2);
 	/*娶到小數點後第二位*/
-	bmi = round(bmi * 100) / 100;
+	bmi = round(bmi * 1000) / 1000.0;
 
 	/*判斷*/
 	if (bmi >= 35) {
-		cout << fixed << setprecision(1) << bmi << " 異常範圍:重度肥胖" << endl;
+		cout << setiosflags(ios::fixed) << setprecision(1) << bmi << " 異常範圍:重度肥胖" << endl;
 	}
 	else if (bmi < 35 && bmi >= 30) {
-		cout << fixed << setprecision(1) << bmi << " 異常範圍:中度肥胖" << endl;
+		cout << setiosflags(ios::fixed) << setprecision(1) << bmi << " 異常範圍:中度肥胖" << endl;
 	}
 	else if (bmi < 30 && bmi >= 27) {
-		cout << fixed << setprecision(1) << bmi << " 異常範圍:輕度肥胖" << endl;
+		cout << setiosflags(ios::fixed) << setprecision(1) << bmi << " 異常範圍:輕度肥胖" << endl;
 	}
 	else if (bmi < 27 && bmi >= 24) {
-		cout << fixed << setprecision(1) << bmi << " 異常範圍:過重" << endl;
+		cout << setiosflags(ios::fixed) << setprecision(1) << bmi << " 異常範圍:過重" << endl;
 	}
 	else if (bmi < 24 && bmi >= 18.5) {
-		cout << fixed << setprecision(1) << bmi << " 正常範圍" << endl;
+		cout << setiosflags(ios::fixed) << setprecision(1) << bmi << " 正常範圍" << endl;
 	}
 	else {
-		cout << fixed << setprecision(1) << bmi << " 體重過輕" << endl;
+		cout << setiosflags(ios::fixed) << setprecision(1) << bmi << " 體重過輕" << endl;
 	}
 }
-
