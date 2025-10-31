026d4e448302c355b81d865654283412de99ee17
DogBrother <roys99141@gmail.com>
2025-10-31 16:14:11 +0800

    又一題

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -1,7 +1,36 @@
 ﻿#include <iostream>
-
+#include <vector>
+#include <sstream>
+#include <string>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n = 0;
+    string input = "";
+    cin >> n;
+	cin.ignore();
+	for (int i = 0; i < n; i++) {
+        vector<float> words;
+		getline(cin, input);
+        std::stringstream ss(input);
+        string word;
+        while (ss >> word) {
+            words.push_back(stof(word));
+        }
+
+        if (words[0] == words[1]) {
+            cout << "DRAW" << endl;
+        }
+        else {
+            if (words[2] == 1) {
+				if (words[0] > words[1]) cout << "A Win" << endl;
+				else cout << "B Win" << endl;
+            }
+            else if(words[2] == -1) {
+                if (words[0] < words[1]) cout << "A Win" << endl;
+                else cout << "B Win" << endl;
+            }
+        }
+	}
 }
 
