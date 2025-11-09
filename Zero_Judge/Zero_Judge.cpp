d2d81e2c752aa88793b27aba2caf1059974def24
DogBrother <roys99141@gmail.com>
2025-11-09 22:42:20 +0800

    居然過了?!

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -3,32 +3,41 @@
 #include <string>
 #include <sstream>
 using namespace std;
-void split(string ,vector<string>&);
-
-void split(string s,vector<string>&result) {
-    //vector<string> result;
-    stringstream ss(s);
-	string word;
-    while (ss >> word) {
-		result.push_back(word);
-    }
-	//return result;
-}
+//void split(string s,vector<string>&result) {
+//    //vector<string> result;
+//    stringstream ss(s);
+//	string word;
+//    while (ss >> word) {
+//		result.push_back(word);
+//    }
+//	//return result;
+//}
 
 int main()
 {
-    vector<string> numbers;
-    string input="";
-    vector<string> result;
-    while (getline(cin,input)) {
-		result.clear();
-        if (input == "-9999") break;
-		split(input, result);
-        for (int i = result.size() - 1; i >= 0; i--) {
-			cout << result[i];
-            if (i != 0) cout << " ";
-            else cout << endl;
+    vector<int> numbers;
+    int input=0;
+    int max = 0;
+    //vector<string> result;
+    while (cin >> input) {
+		numbers.push_back(input);
+		if (input > max) {
+			max = input;
+		}
+    }
+
+    for (int num : numbers) {
+		int de = max - num/2;
+        for (int i = 0; i < de; i++) {
+			cout << " ";
         }
+        for (int j = 0; j < num; j++) {
+			cout << "*";
+        }
+        for (int i = 0; i < de; i++) {
+            cout << " ";
+        }
+		cout << endl;
     }
 }
 
