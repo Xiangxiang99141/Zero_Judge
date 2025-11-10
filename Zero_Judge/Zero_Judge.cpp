566617c1ffa8e9481c38c350e4f40b86fff7af03
DogBrother <roys99141@gmail.com>
2025-11-11 07:56:15 +0800

    不小心想一個太複雜了

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -1,7 +1,37 @@
 ﻿#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+#include <functional>
 using namespace std;
+
+void split_number_string(vector<int>& v)
+{
+	string t;
+	getline(cin, t);
+	stringstream ss(t);
+	string word;
+	while (ss >> word)
+	{
+		v.push_back(stoi(word));
+	}
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    string s = "";
+    getline(cin,s);
+    vector<int>pass_numbers;
+	split_number_string(pass_numbers);
+	sort(pass_numbers.begin(), pass_numbers.end(), less<int>());
+	const int str_len = s.size();
+	string new_str = "";
+	for (int i = 0; i < str_len; i++) {
+		//找當前位置是否在pass_numbers中
+		if (find(pass_numbers.begin(), pass_numbers.end(), i+1) == pass_numbers.end()) {
+			new_str += s[i];
+		}
+	}
+	cout << new_str << endl;
 }
 
