c6c4cedb93e4c6feb20af95a3d8b4fdd45f4e2ab
DogBrother <roys99141@gmail.com>
2025-11-11 14:21:19 +0800

    測資居然有中文

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -1,7 +1,35 @@
 ﻿#include <iostream>
+#include <cctype>
+#include <string>
+//#include <algorithm>
 using namespace std;
+//string a[] = { "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" };
 int main()
 {
-    std::cout << "Hello World!\n";
+    string input="";
+    while (cin >> input) {
+        //string new_str = "";
+        for (char c : input) {
+			bool isUpper = isupper(c);
+            char lower_c = tolower(c);
+            if (isUpper) {
+                if (int(c)-3 < 65) {
+					cout << (char)(90-65-(c - 3));
+				}
+				else {
+					cout << (char)(c - 3);
+                }
+			}
+			else {
+                if (int(c) - 3 < 97) {
+                    cout << (char)(122-97-(c - 3));
+                }
+                else {
+                    cout << (char)(c - 3);
+                }
+            }
+        }
+		cout << endl;
+    }
 }
 
