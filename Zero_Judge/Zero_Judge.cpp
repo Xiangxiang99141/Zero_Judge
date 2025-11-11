a440a3e60cae3335783e26f24f4f3a077e73d514
DogBrother <roys99141@gmail.com>
2025-11-11 14:40:46 +0800

    奇怪了，怎麼都沒輸出

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -1,22 +1,30 @@
 ﻿#include <iostream>
 using namespace std;
-bool isPrime(int number);
+bool isPrime(int);
 int main()
 {
     int x = 0;
     while (cin >> x) {
-        if (x == 1) {
+        if (isPrime(x)) {
+			cout << x << "是質數" << endl;
+        }
+        else {
 			cout << x << "不是質數" << endl;
         }
-		else cout << x << (isPrime(x) ? "是質數" : "不是質數") << endl;
     }
     
 }
 bool isPrime(int number)
 {
-    for (int i = 2; i < number - 1; i++) {
-		if (number % i == 0)
-			return false;
+    if (number <= 1) return false;
+    if (number <= 3) return true;
+    if (number % 2 == 0 || number % 3 == 0) {
+        return false;
+    }
+    for (int i = 5; i * i <= number; i = i + 6) { // 從 5 開始，步長為 6
+        if (number % i == 0 || number % (i + 2) == 0) {
+            return false;
+        }
     }
     return true;
 }
