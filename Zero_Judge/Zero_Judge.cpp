390a9eba5b67f7ae31fe3589b54d44f63caa85da
DogBrother <roys99141@gmail.com>
2025-12-11 09:31:10 +0800

    題目有bug，先跳過

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -4,26 +4,27 @@ bool isPrime(int);
 int main()
 {
     int x = 0;
-    while (cin >> x) {
-        if (isPrime(x)) {
-			cout << x << "是質數" << endl;
-        }
-        else {
-			cout << x << "不是質數" << endl;
-        }
+    cin >> x;
+    if (isPrime(x)) {
+		cout << x << "是質數" << endl;
+    }
+    else {
+		cout << x << "不是質數" << endl;
     }
     
 }
 bool isPrime(int number)
 {
     if (number <= 1) return false;
-    if (number <= 3) return true;
-    if (number % 2 == 0 || number % 3 == 0) {
+    else if (number <= 3) return true;
+    else if (number % 2 == 0 || number % 3 == 0) {
         return false;
     }
-    for (int i = 5; i * i <= number; i = i + 6) { // 從 5 開始，步長為 6
-        if (number % i == 0 || number % (i + 2) == 0) {
-            return false;
+    else {
+        for (int i = 5; i * i <= number; i = i + 6) { // 從 5 開始，步長為 6
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
         }
     }
     return true;
