2015e3d98d0f563baf25c82e612326c37e14b7b9
DogBrother <roys99141@gmail.com>
2025-12-11 10:01:58 +0800

    一次就過

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -1,7 +1,20 @@
 ﻿#include <iostream>
+#include<vector>
+#include <algorithm>
 using namespace std;
+vector<string> card = { "1","2","3","4","5","6","7","8","9","10","J","Q","K" };
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n;
+    string f[2] = {"0","0"};
+	std::cin >> n;
+    int sum[2] = { 0,0 };
+    for (int i = 0; i < n; i++) {
+		cin >> f[0] >> f[1];
+        int  card1 = std::distance(card.begin(),find(card.begin(), card.end(), f[0]))+1; //小明
+		auto card2 = std::distance(card.begin(), find(card.begin(), card.end(), f[1]))+1;
+		card1 > card2 ? (sum[1] += card1 - card2) : (sum[0] += card2 - card1);
+    }
+    cout << sum[0] << " " << sum[1];
 }
 
