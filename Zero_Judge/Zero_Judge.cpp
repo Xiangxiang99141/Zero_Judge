0ba71b6b4c0082173ec0ba438bbb61da73bfc1ae
dogbrother <roys99141@gmail.com>
2026-03-10 01:55:12 +0800

    完成

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -1,7 +1,36 @@
 ﻿#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
 using namespace std;
+void split(string, int[]);
 int main()
 {
-    std::cout << "Hello World!\n";
+    string num = "";
+    int nums[3] = { 0 };
+    while (cin >> num) {
+        //cout << num;
+        split(num, nums);
+        nums[2] = abs(nums[0] + nums[1]);
+        cout << nums[0] << "," << nums[1] << "," << nums[2];
+        cout << endl;
+    }
+}
+
+void split(string str, int ns[]) {
+    auto pt = str.find(".");
+    if (pt != std::string::npos) {
+		ns[0] = stoi(str.substr(0, pt));
+        string t = str.substr(pt + 1);
+        size_t first = t.find_first_not_of('0');
+        if(first != std::string::npos) {
+            size_t last = t.find_last_not_of('0');
+            ns[1] = stoi(t.substr(first, last - first + 1));//只擷取前後不是0的區塊
+        }
+        else ns[1] = 0;
+    }
+    else {
+        ns[0] = stoi(str);
+    }
 }
 
