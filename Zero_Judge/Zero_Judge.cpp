4cd8c51a56f5dbc22015823929262f9558ae9c0b
dogbrother <roys99141@gmail.com>
2026-03-16 15:26:22 +0800

    天干地支的變化提

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -6,18 +6,15 @@ using namespace std;
 int main()
 {
     int year = 0;
-    std::string a[10] = { "甲","乙","丙","丁","戊","己","庚","辛","壬","癸" };
-    std::string b[12] = { "子","丑","寅","卯","辰","巳","午","未","申","酉","戌","亥"};
-    int basic_year = 1954;
-    int basic_posistion[2] = { 0,6 };
+    std::string a[12] = { "鼠","牛","虎","兔","龍","蛇","馬","羊","猴","雞","狗","豬" };
+    int basic_year = 2025;
+    int basic_posistion =  5 ;
     while (cin >> year) {
         //判斷與基本年差距
 		int diff = year - basic_year;
         //計算循環幾次跟榆樹
-        int ten_gan = ((diff % 10)+10)%10;
-        int float_zhi = ((basic_posistion[1] + diff % 12) + 12)%12; //雙取榆樹讓數字變為正數
-        cout << a[ten_gan] << b[float_zhi];
-        cout << endl;
+        int float_zhi = ((basic_posistion + diff % 12) + 12)%12; //雙取榆樹讓數字變為正數
+        cout << a[float_zhi] << endl;
     }
 }
 
