aecb52ae00d93e720bcd1086628853d51111f3a3
dogbrother <roys99141@gmail.com>
2026-03-16 15:46:16 +0800

    好像也還行

--- Zero_Judge/Zero_Judge.cpp
+++ Zero_Judge/Zero_Judge.cpp
@@ -1,7 +1,15 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	string input;
+	while (cin >> input) {
+		string hexcolor = input.substr(1);
+		int r = stoi(hexcolor.substr(0, 2), nullptr, 16);
+		int g = stoi(hexcolor.substr(2, 2), nullptr, 16);
+		int b = stoi(hexcolor.substr(4, 2), nullptr, 16);
+		cout << r << " " << g << " " << b << endl;
+	}
 }
 
